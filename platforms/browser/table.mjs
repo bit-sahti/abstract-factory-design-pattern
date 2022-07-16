d36a52dc04f004507e249aa3f1419feb6bdfbf23
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-07-16 07:03:50 -0300

    feat: render table on browser

--- platforms/browser/table.mjs
+++ platforms/browser/table.mjs
@@ -1,7 +1,61 @@
 import { TableComponent } from '../../shared/base/tableComponent.mjs'
 
 export class BrowserTableComponent extends TableComponent {
-  render(data) {
-    window.alert('on browser render')
+  render(data) {    
+    const table = this.#buildTable(data)
+
+    document.body.insertAdjacentHTML('afterBegin', table)
+
+    // console.log(table)
+  }
+
+  #buildTable(data) {    
+    const { tableHeaders, tableBody } = this.#transformData(data) 
+
+    return `
+      <table class="table">
+        <thead>
+          <tr>
+            ${tableHeaders}
+          </tr>
+        </thead>
+        <tbody>
+          ${tableBody}
+        </tbody>
+      </table>
+    `
+  }
+
+  #transformData(data) {
+    const headersData = Object.keys(data[0])
+    const rowsData = data.map(document => Object.values(document))
+
+    const tableHeaders = this.#buildHeaders(headersData)
+
+    const tableBody = this.#buildBody(rowsData)
+
+    return {
+      tableHeaders,
+      tableBody,
+    }
+  }
+
+  #buildHeaders(headersData) {
+    return headersData
+            .map(header => `<th scope="col">${header.toUpperCase()}</th>`)
+            .join('')
+  }
+
+  #buildBody(rowsData) {
+    return rowsData
+            .map(row => this.#buildColumns(row))
+            .map(row => `<tr>${row}</tr>`)
+            .join('')
+  }
+
+  #buildColumns(columnsData) {
+    return columnsData
+            .map(column => `<td>${column}</td>`)
+            .join('')
   }
 }
