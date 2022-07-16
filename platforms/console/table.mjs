298a8d4d7c9b2ac371c48d9574a8ecf655506e1a
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-07-16 13:05:15 -0300

    feat: render table on console

--- platforms/console/table.mjs
+++ platforms/console/table.mjs
@@ -1,7 +1,35 @@
+import chalk from 'chalk'
+import chalkTable from 'chalk-table'
 import { TableComponent } from '../../shared/base/tableComponent.mjs'
 
 export class ConsoleTableComponent extends TableComponent {
   render(data) {
-    console.log('on console render', data)
-  } 
+    const template = this.#buildTable(data)
+
+    console.log(template)
+  }
+
+  #buildTable(data) {
+    const headers = Object.keys(data[0])
+
+    const options = this.#getOptions(headers)
+
+    return chalkTable(options, data)
+  }
+
+  #getOptions(headers) {
+    const columns = headers.map((header, i) => ({
+      field: header,
+      name: this.#getColor(i, header),
+    }))
+
+    return {
+      leftPad: 2,
+      columns,
+    }
+  }
+
+  #getColor(i, name) {
+    return i % 2 === 0 ? chalk.red(name) : chalk.cyanBright(name)
+  }
 }
